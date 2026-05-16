cab465f33f74ddf2ae5c773112df81a9e8847bf1
JagadeeswarJ <jagadeeswar079@gmail.com>
2026-05-16 07:51:18 +0530

    .

--- external/practice/arime.py
+++ external/practice/arime.py
@@ -2,4 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
-pd.read_csv('sales.csv')
+data = pd.read_csv('sales.csv')
+
+series = data['Sales']
+
+plt.plot(series)
+plt.title("Original Data")
+plt.show()
+
+
+model =  ARIMA(series,order = (1,1,1))
+
+model_fit = model.fit()
+
+forecast = model_fit.forecast(steps=5)
+
+plt.plot(series, label='Original')
+
+plt.plot(range(len(series), len(series) + 5), forecast, label='Forecast')
+plt.legend()
+plt.show()
